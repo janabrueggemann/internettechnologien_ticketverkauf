386a77b82d65616f43ee896e6f9bb7c56a51bcd6
aylinx04 <165890967+aylinx04@users.noreply.github.com>
2025-06-18 19:06:28 +0200

    ich hasse mein leben
    wie leiden zusammen 👎
    
    Co-Authored-By: SaZahed <165900610+SaZahed@users.noreply.github.com>
    Co-Authored-By: janabrueggemann <166311714+janabrueggemann@users.noreply.github.com>
    Co-Authored-By: nhakari <207917497+noonooch@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,33 @@
-
+import {RouterOutlet, RouterLink, RouterLinkActive} from '@angular/router';
 import { RouterModule, Routes } from '@angular/router';
 import { StartseiteComponent } from './startseite/startseite.component';
 import { AboutComponent } from './about/about.component';
 import { WarenkorbComponent } from './warenkorb/warenkorb.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { NgModule } from '@angular/core';
 
 
-const routes: Routes = [
+
+export const routes: Routes = [
   { path: '', component: StartseiteComponent }, // Standardroute
   { path: 'about', component: AboutComponent },
   { path: 'warenkorb', component: WarenkorbComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-
 ];
 
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+}) 
 export class AppRoutingModule { }
+
+//@Component({
+//selector: 'app-root‘,
+//imports: [RouterOutlet, RouterLink, RouterLinkActive],
+//templateUrl: './app.component.html‘,
+//styleUrls: ['./app.component.css‘]})
+//export class AppComponent {
+//title = 'routing-app‘;
+//}
